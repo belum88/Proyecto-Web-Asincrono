9b36076fa1813557e09c104411aa1a8c400db162
Abel <abel.fernandez.martinez@gmail.com>
2024-12-08 01:44:14 +0100

    Initial commit

---
+++ components/nav/nav.js
@@ -0,0 +1,41 @@
+import './nav.css'
+
+export const navbar = () => 
+    `<nav id="navbar">
+          <ul>
+            <li>
+              <img src="./icons/pinterest-logo.png" class="logo" alt="pinterest logo">
+            </li>
+            <li>
+              <button type="button" class="select-button home">Home</button>
+            </li>
+            <li>
+              <button type="button" class="select-button create">Create</button>
+            </li>  
+            <li class="search-container">
+              <form id="search-form">
+                <input type="text" id="search" placeholder="Search..." />
+              </form>  
+            </li>  
+              <div class="icons">
+                <li>
+                  <button type="button">
+                    <img src="./icons/notification-icon.png" class="button-icon" alt="notification icon">
+                  </button>
+                </li> 
+                <li>
+                  <button type="button">
+                    <img src="./icons/dm-icon.png" class="button-icon" alt="direct messages icon">
+                  </button>
+                </li>  
+                <li>
+                  <button type="button">
+                    <img src="./icons/profile-icon.png" class="button-icon" alt="profile icon">
+                  </button>
+                </li>  
+              </div>
+          </ul>
+        </nav>`;
+
+
+ 
---
+++ data/data.js
@@ -0,0 +1,78 @@
+import { galleryGrid } from "../components/gallery/gallery.js";
+import { modal } from "../components/modal/modal.js";
+import { nextPage } from "../components/footer/footer.js";
+
+const gallery = document.querySelector("#image-grid");
+const footer = document.querySelector("footer");
+footer.innerHTML = nextPage();
+
+let currentPage = 1;
+let lastUserInput;
+
+export const fetchPhotos = async (
+  userInput = "technology",
+  page = 1,
+  perPage = 24
+) => {
+  const accessKey = "PExlSM_mtUUZj1H9nmIaD5tFsHTiRfJkEYR8EO8rr4Y";
+
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/search/photos?query=${userInput}&page=${page}&per_page=${perPage}&client_id=${accessKey}`
+    );
+    const allPhotos = await res.json();
+    console.log(allPhotos);
+    const photosArray = allPhotos.results;
+
+    gallery.innerHTML = "";
+
+    photosArray.forEach((photo) => {
+      gallery.innerHTML += galleryGrid(photo);
+    });
+
+    if (photosArray.length === 0) {
+      gallery.innerHTML = modal();
+      footer.innerHTML = "";
+
+      const suggestedButtons = document.querySelectorAll(".suggested-button");
+      suggestedButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          const inputValue = button.textContent;
+          fetchPhotos(inputValue, currentPage);
+          footer.innerHTML = nextPage();
+          flipPage();
+        });
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching photos:", error);
+  }
+};
+
+export const renderPhotos = () => {
+  const searchForm = document.querySelector("#search-form");
+  const input = document.querySelector("#search");
+
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    lastUserInput = input.value;
+    console.log(lastUserInput);
+    if (lastUserInput) {
+      currentPage = 1;
+      fetchPhotos(lastUserInput, currentPage);
+    } else {
+      console.log("No photos found.");
+    }
+  });
+};
+
+export const flipPage = () => {
+  const nextPageButton = document.querySelector(".next-page-button");
+  const nextPageText = document.querySelector(".next-page-text");
+
+  nextPageButton.addEventListener("click", () => {
+    currentPage++;
+    nextPageText.textContent = currentPage;
+    fetchPhotos(lastUserInput, currentPage);
+  });
+};
